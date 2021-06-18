346c71136a734501e97eadb7665b8951d03e44f4
evanpan117 <haojie@csu.fullerton.edu>
2021-06-17 17:06:00 -0700

    v3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import importImg from './img.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={importImg} alt='import' width="500" height="600"></img>
         <p>
           Site of Realtor Evan Pan
         </p>
